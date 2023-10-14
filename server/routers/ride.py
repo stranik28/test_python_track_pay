445d8c5ada09de5b3d4910193cf3a98bfb7031e3
stanik <jb357506@gmail.com>
2023-10-14 11:51:37 +0300

    Add catch

--- server/routers/ride.py
+++ server/routers/ride.py
@@ -8,7 +8,8 @@ from api.response.ride import RideResponse, RideResponseFactory, RideHistoryResp
 from db.models.rides import DBRide
 from managers.ride import RideManager
 from server.depends import get_auth_account_id, get_session, PagesPaginationParams
-from vendors.exception import BluetoothNotFound, AccessDenied, RideNotFound, UserNotFound, NotSureToCreateRide
+from vendors.exception import BluetoothNotFound, AccessDenied, RideNotFound, UserNotFound, NotSureToCreateRide, \
+    RideAlreadyDone
 
 router = APIRouter(prefix="/ride", tags=['Ride'])
 
@@ -73,9 +74,8 @@ async def touch(
         HTTPException(status_code=404, detail="Пользователь с таким uuid не найден ")
     except NotSureToCreateRide:
         HTTPException(status_code=404, detail="Недостаточно пока оснований для создания поездки")
-    except :
+    except RideAlreadyDone:
         HTTPException(status_code=400, detail="Не найденно доверненного устрйоства с таким id")
-    except
     print(f"Ought uuid is {uuid} from esp {esp_id}")
     return {"result": uuid}
 
