2014d318defb8d916d8680a46d18222aa1985505
stanik <jb357506@gmail.com>
2023-10-14 13:03:30 +0300

    Add comment

--- managers/ride.py
+++ managers/ride.py
@@ -109,8 +109,9 @@ class RideManager:
             raise EspNotFound
 
         esp = esp[0]
-
-        last_ride = await RideRepository(session).get_full_ride_history(user_id=user_exist.id, limit=1, offset=0)
+        timedelta_ = datetime.timedelta(hours=1)
+        last_ride = await RideRepository(session).get_full_ride_history(user_id=user_exist.id, timedelta_=timedelta_,
+                                                                        transport_id=esp.transport.id, limit=1, offset=0)
 
         if last_ride != []:
             print("Уже покатался")
