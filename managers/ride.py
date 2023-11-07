679282545b049b1f3b472968c839fa03f8bb0725
Nikita <jb357506@gmail.com>
2023-11-08 00:37:43 +0300

    FIx time

--- managers/ride.py
+++ managers/ride.py
@@ -116,7 +116,7 @@ class RideManager:
         user_exist = user_exist[0]
         await RideRepository(session).add_touch(uuid=uuid, esp_id=esp_id)
         # timdelta = datetime.timedelta(minutes=10)
-        timdelta = datetime.timedelta(hours=3, seconds=40)
+        timdelta = datetime.timedelta(seconds=40)
         last_touches: list[int] = await RideRepository(session).count_last_touche(uuid=uuid, time=timdelta)
 
         if last_touches != []:
@@ -131,7 +131,7 @@ class RideManager:
             raise EspNotFound
 
         esp = esp[0]
-        timedelta_ = datetime.timedelta(hours=3, minutes=10)
+        timedelta_ = datetime.timedelta(minutes=10)
         last_ride = await RideRepository(session).get_full_ride_history(user_id=user_exist.user_id, timedelta_=timedelta_,
                                                                         transport_id=esp.transport.id, limit=1, offset=0)
 
